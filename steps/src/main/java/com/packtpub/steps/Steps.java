464fadcebd6f1504cbb5ebeaccdfa9b7de74476b
nesterenko-marina99 <miss.nesterenko.marina@gmail.com>
2021-08-19 12:18:43 +0400

    exercise 7 is done

---
+++ steps/src/main/java/com/packtpub/steps/Steps.java
@@ -0,0 +1,29 @@
+package com.packtpub.steps;
+
+import java.time.LocalDate;
+
+public class Steps {
+    private int steps;
+    private LocalDate date;
+
+    public Steps(int steps, LocalDate date) {
+        this.steps = steps;
+        this.date = date;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
