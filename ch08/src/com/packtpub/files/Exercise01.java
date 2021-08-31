5ce66c5df520191a452a81cc866c19d13a2b859c
nesterenko-marina99 <miss.nesterenko.marina@gmail.com>
2021-08-31 20:54:46 +0400

    ch08ex1 task6 is done

--- ch08/src/com/packtpub/files/Exercise01.java
+++ ch08/src/com/packtpub/files/Exercise01.java
@@ -11,6 +11,12 @@ public class Exercise01 {
         String pathString = System.getProperty("user.home");
         Path path = Paths.get(pathString);
         Stream<Path> fileNames = Files.list(path).filter(Files::isDirectory);
-        fileNames.limit(5).forEach(System.out::println);
+        fileNames.limit(5).forEach((item) -> {
+            System.out.println(item.toString());
+            try {
+                Stream<Path> fileNames2 = Files.list(item).filter(Files::isDirectory);
+                fileNames2.forEach(System.out::println);
+            } catch (IOException ioe) {}
+        });
     }
 }
