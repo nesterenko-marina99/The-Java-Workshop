827195832d7c1238f03571a2015324f0b8659a9a
nesterenko-marina99 <miss.nesterenko.marina@gmail.com>
2021-08-31 21:11:01 +0400

    ch08ex1 task9 is done

--- ch08/src/com/packtpub/files/Exercise01.java
+++ ch08/src/com/packtpub/files/Exercise01.java
@@ -2,6 +2,7 @@ package com.packtpub.files;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,13 +11,13 @@ public class Exercise01 {
     public static void main(String[] args) throws IOException {
         String pathString = System.getProperty("user.home");
         Path path = Paths.get(pathString);
-        Stream<Path> fileNames = Files.list(path).filter(Files::isDirectory);
-        fileNames.limit(5).forEach((item) -> {
-            System.out.println(item.toString());
-            try {
-                Stream<Path> fileNames2 = Files.list(item).filter(Files::isDirectory);
-                fileNames2.forEach(System.out::println);
-            } catch (IOException ioe) {}
+        Files.walkFileTree(path, Collections.emptySet(), 2, new SimpleFileVisitor<Path>()
+        {
+            @Override
+            public FileVisitResult preVisitDirectory (Path dir, BasicFileAttributes attr) {
+                System.out.println(dir.toString());
+                return FileVisitResult.CONTINUE;
+            }
         });
     }
 }
